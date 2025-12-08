11b6d80c50234c5acdc0fddfb0ff405a06f64ce6
Trickster <Mehdi.Tarissi@emsi-edu.ma>
2025-12-08 12:36:36 +0100

    Target Customer's endpoints from smp to test if it's working

--- src/main/java/com/demo/demo/smp/web/ProductController.java
+++ src/main/java/com/demo/demo/smp/web/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/customers")
 public class ProductController {
 
     private final CustomerRepository repo;
